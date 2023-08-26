0cc3a14891a61c0ce197c4e78c6870fc75958e20
K1rLes <kirill.lesovoi@mail.ru>
2023-08-27 03:10:58 +0500

    Обработка Not modified исключения, новые версии библиотек

--- bot/handlers/errors.py
+++ bot/handlers/errors.py
@@ -1,15 +1,30 @@
 from typing import TYPE_CHECKING
 
-from aiogram import Router
+from aiogram import F, Router
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.filters import ExceptionTypeFilter
 from loguru import logger
 
 if TYPE_CHECKING:
     from aiogram.types import ErrorEvent
 
+
 router = Router(name=__name__)
 
 
+@router.errors(
+    ExceptionTypeFilter(TelegramBadRequest),
+    F.exception.message.contains("is not modified"),
+)
+async def not_modified_error(_: "ErrorEvent") -> None:
+    """Сообщение не было изменено.
+
+    Возникает, если нажать "сегодня" при просмотре расписаний на сегодня.
+    """
+    logger.debug("Message is not modified")
+
+
 @router.errors()
-async def all_errors(exception: "ErrorEvent") -> None:
+async def all_errors(event: "ErrorEvent") -> None:
     """Логгер всех ошибок при обработке событий телеграма."""
-    logger.error(f"Exception Error: {exception.exception}, {exception.update}")
+    logger.error(f"Exception while handling: {event.exception} | {event.update}")
