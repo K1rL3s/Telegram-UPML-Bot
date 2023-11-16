05fa2489a8ec1e9b23a072e0cec003aa0ff6f842
IliaMax80 <96135599+IliaMax80@users.noreply.github.com>
2023-11-16 18:48:40 +0500

    add class educators add room cleaning

--- bot/upml/educators_excel.py
+++ bot/upml/educators_excel.py
@@ -1,3 +1,5 @@
+import functools
+
 import asyncio
 import datetime as dt
 from pathlib import Path
@@ -26,52 +28,94 @@ async def load_educators_from_xlsx(repo: "EducatorsScheduleRepository") -> None:
     while current_date.month == today.month:
         educators_names = parse_excel(current_date.day)
         schedule = format_text(educators_names)
-
         await repo.save_or_update_to_db(
             date=current_date,
             schedule_text=schedule,
         )
-
         current_date += dt.timedelta(days=1)
-
 
 def parse_excel(day: int) -> list[str]:
     wb: Workbook = load_workbook(Path().cwd() / "resources" / "educators.xlsx")
+    wb_room: Workbook = load_workbook(Path().cwd() / "resources" / "educators_room.xlsx")
     ws: Worksheet = wb.active
+    ws_room: Worksheet = wb_room.active
 
-    list_educators: list[str] = []
-    index_educarors: int = 3
-    work_cell: cell = ws.cell(index_educarors, day + 2)
+    list_educators: list[Educator] = []
+    index_educators: int = 3
+    work_cell: cell = ws.cell(index_educators, day + 2)
 
     while not work_cell.value is None:
         if "В" not in work_cell.value:
-            list_educators.append(work_cell.value + " " + ws.cell(index_educarors, 1).value)
-        index_educarors += 1
-        work_cell = ws.cell(index_educarors, day + 2)
+            educator: Educator = Educator(day, index_educators, ws, ws_room)
+            list_educators.append(educator)
+        index_educators += 1
+        work_cell = ws.cell(index_educators, day + 2)
 
     list_educators.sort(key=educators2time)
     today = date_today()
-    day = dt.date(year=today.year, month=today.month, day=day).replace(1).day
-    index_educarors = 3
-    work_cell: cell = ws.cell(index_educarors, day + 2)
+    day = dt.date(year=today.year, month=today.month, day=day) - dt.timedelta(1)
+    day = day.day
+    index_educators = 3
+    work_cell = ws.cell(index_educators, day + 2)
 
     while not work_cell.value is None:
         if "В" not in work_cell.value:
             if work_cell.value.split("-")[1][0] == "9":
-                (list_educators.insert(0, work_cell.value + " " + ws.cell(index_educarors, 1).value))
-        index_educarors += 1
-        work_cell = ws.cell(index_educarors, day + 2)
+                educator: Educator = Educator(day, index_educators, ws, ws_room)
+                list_educators.insert(0, educator)
+                list_educators[0].barder_start_time()
+        index_educators += 1
+        work_cell = ws.cell(index_educators, day + 2)
 
-    list_educators = map(format_educators_line, list_educators)
-    return list_educators
-def format_educators_line(line: str) -> str:
-    list_line = line.split()
-    line = list_line[0].split("-")[0] + ":00-" + list_line[0].split("-")[1] + ":00 "
-    line += list_line[1] + " " + list_line[2] + " " + list_line[3]
-    return line
-def educators2time(educators: str) -> int:
-    time = educators.split("-")[0]
-    return int(time)
+    isrooms: bool = list_educators[0].isroom and list_educators[1].isroom
+    if isrooms and list_educators[0].room > list_educators[1].room:
+        list_educators[0], list_educators[1] = list_educators[1], list_educators[0]
+    isrooms: bool = list_educators[-1].isroom and list_educators[-2].isroom
+    if isrooms and list_educators[-2].room > list_educators[-1].room:
+        list_educators[-2], list_educators[-1] = list_educators[-1], list_educators[-2]
+
+    #map(functools.partial(Educator.compose_string, isname=True, isnickname=True), list_educators)
+
+    list_text_educators: list[str] = map(Educator.compose_string, list_educators)
+    return list_text_educators
+
+class Educator:
+    def __init__(self, day: int, index_educators: int, ws: Worksheet, ws_room: Worksheet):
+        time = str(ws.cell(index_educators, day + 2).value).split("-")
+        self.time_start: str = time[0]
+        self.time_end: str = time[1]
+        self.name: str = ws.cell(index_educators, 1).value
+        self.nickname: str = ws.cell(index_educators, 2).value
+        self.isroom: bool = not ws_room.cell(index_educators - 1, day + 1).value is None
+        if self.isroom:
+            self.room: int = int(ws_room.cell(index_educators - 1, day + 1).value)
+
+    def compose_string(self, isname: bool = True, isnickname: bool = False) -> str:
+        self.format_time()
+        line: str = "<b>" + self.time_start + "-" + self.time_end + "</b> "
+        if isnickname and isname:
+            line += self.name + " (" + self.nickname + ")"
+        elif isnickname:
+            line += self.nickname
+        else:
+            line += self.name
+        if self.isroom:
+            line += " ("
+            line += "3-6" if self.room == 59 else "7-9"
+            line += " этаж)"
+        return line
+    def barder_start_time(self) -> None:
+        self.time_start = "00"
+    def format_time(self) -> str:
+        if int(self.time_start) < int(self.time_end):
+            self.time_start = self.time_start + ":00"
+            self.time_end = self.time_end + ":00"
+        else:
+            self.time_start = self.time_start + ":00"
+            self.time_end = "23:59"
+
+def educators2time(educators: Educator) -> int:
+    return int(educators.time_start)
 def format_text(educators_names: list[str]) -> str:
     return '\n'.join(educators_names)
 
